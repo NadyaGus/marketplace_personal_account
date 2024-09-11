af93c5d191e96e597f6f7faa85274b02b81943d1
NadyaGus <ftotkexit@gmail.com>
2024-09-11 21:19:15 +0300

    feat: add loader

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,5 +1,7 @@
-import type { ReactNode } from 'react';
-import { Outlet } from 'react-router-dom';
+import { type ReactNode, useEffect } from 'react';
+import { Outlet, useNavigation } from 'react-router-dom';
+
+import { Loader, LoadingOverlay } from '@mantine/core';
 
 import { Header } from '@/widgets/header';
 
@@ -8,10 +10,17 @@ import { APP_ROUTES } from '../routers';
 import classes from './layout.module.css';
 
 export const Layout = (): ReactNode => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    console.log(navigation.state);
+  }, [navigation.state]);
+
   return (
     <>
       <Header links={[APP_ROUTES.allAdvertisements, APP_ROUTES.orders]} />
 
+      <LoadingOverlay loaderProps={{ children: <Loader /> }} pos={'fixed'} visible={navigation.state === 'loading'} />
       <main className={classes.main}>
         <Outlet />
       </main>
